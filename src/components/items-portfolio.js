4def6964bdbb1e3c36eed86c00c457be9e6277bd
johan gace <gace.johan3@gmail.com>
2022-04-17 20:52:18 -0400

    portfolio

--- src/components/items-portfolio.js
+++ src/components/items-portfolio.js
@@ -6,9 +6,9 @@ import '../style/list-portfolio.less';
 class PortfolioItem extends React.Component {
 	render() {
 		return (
-			<div className="col m6">
+			<div className="col m12 l6">
 				<div className="row flex">
-					<div className="col m6 l12 image orange ">
+					<div className="col m12  image orange ">
 						<Link
 							to={this.props.data.node.fields.slug}
 							title={this.props.data.node.frontmatter.title}
