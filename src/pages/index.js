7fc98469d65b530ba78d49b2d15b8872f57a76e4
johan gace <gace.johan3@gmail.com>
2022-04-21 22:25:28 -0400

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,47 @@
-import React from "react";
-import Layout from "../components/layout";
-import { graphql } from "gatsby";
-import SEO from "../components/seo";
-import SocialLinks from "../components/sociallinks";
-import BlogList from "../components/list-blog";
-import "../style/wall.less";
-var ReactRotatingText = require("react-rotating-text");
+import React from 'react';
+import Layout from '../components/layout';
+import { graphql } from 'gatsby';
+import SEO from '../components/seo';
+import SocialLinks from '../components/sociallinks';
+import BlogList from '../components/list-blog';
+import '../style/wall.less';
+import Helmet from 'react-helmet';
+
+var ReactRotatingText = require('react-rotating-text');
 
 class IndexPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            winHeight: "100vh",
-        };
-        // this.width = window.innerWidth;
-        if (typeof window === "undefined") {
-            global.window = {};
-        }
-        // const module = typeof window !== `undefined` ? require("module") : null;
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			winHeight: '100vh'
+		};
+		// this.width = window.innerWidth;
+		if (typeof window === 'undefined') {
+			global.window = {};
+		}
+		// const module = typeof window !== `undefined` ? require("module") : null;
+	}
 
-    createSVGElement(n, v) {
-        n = document.createElementNS("http://www.w3.org/2000/svg", n);
-        for (var p in v) n.setAttributeNS(null, p, v[p]);
-        return n;
-    }
+	createSVGElement(n, v) {
+		n = document.createElementNS('http://www.w3.org/2000/svg', n);
+		for (var p in v) n.setAttributeNS(null, p, v[p]);
+		return n;
+	}
 
-  
-    setWindowHeight() {
-        this.setState({
-            winHeight: window.innerHeight,
-        });
-    }
-    render() {
-        return typeof window !== `undefined` ? (
-            <Layout placeholder={false}>
-                <SEO
-                    lang="en"
-                    title={this.props.data.site.siteMetadata.title}
-                />
-                <div
-                    className="wall"
-                    style={{ height: this.state.winHeight + "px" }}
-                >
-                    <div className="intro container">
-                        <div className="tag-line text-secondary">
-                            {/* <svg
+	setWindowHeight() {
+		this.setState({
+			winHeight: window.innerHeight
+		});
+	}
+	render() {
+		return typeof window !== `undefined` ? (
+			<Layout placeholder={false}>
+				<SEO lang="en" title={this.props.data.site.siteMetadata.title} />
+                
+				<div className="wall" style={{ height: this.state.winHeight + 'px' }}>
+					<div className="intro container">
+						<div className="tag-line text-secondary">
+							{/* <svg
                                 width="10%"
                                 height="205px"
                                 viewBox="30 30 30 30"
@@ -92,68 +88,93 @@ class IndexPage extends React.Component {
                                               .title}
                                 </text>
                             </svg> */}
-                            <p className="tag-line text-secondary">
-                                <span className="red bkg-blk"> JOHAN</span>{" "}
-                                GACE.
-                                {/* {this.props.data.site.siteMetadata.title.toUpperCase()} */}
-                            </p>
-                        </div>
-                        {/* <span>.</span> */}
+							<p className="tag-line text-secondary">
+								<span className="red bkg-blk"> JOHAN</span> GACE.
+								{/* {this.props.data.site.siteMetadata.title.toUpperCase()} */}
+							</p>
+						</div>
+						{/* <span>.</span> */}
 
-                        <h1 className="caption text-secondary ">
-                            <ReactRotatingText
-                                typingInterval={120}
-                                color
-                                deletingInterval={80}
-                                pauseInterval={8000}
-                                // eraseMode={"overwrite"}
-                                items={[
-                                    "I am a UX Engineer.",
-                                    "I craft Digital Experiences.",
-                                    "I Design & Build things.",
-                                ]}
-                            />
-                        </h1>
-                        <div className="container">
-                            {/* <hr className="v-line" /> */}
-                            <a className="arrow ">............</a>
-                        </div>
-                    </div>
+						<h1 className="caption text-secondary ">
+							<ReactRotatingText
+								typingInterval={120}
+								color
+								deletingInterval={80}
+								pauseInterval={8000}
+								// eraseMode={"overwrite"}
+								items={[
+									'I am a UX Engineer.',
+									'I craft Digital Experiences.',
+									'I Design & Build things.'
+								]}
+							/>
+						</h1>
+						<div className="container">
+							{/* <hr className="v-line" /> */}
+							<a className="arrow ">............</a>
+						</div>
+					</div>
 
-                    {/* <a href="#portfolio" className="arrow">
+					{/* <a href="#portfolio" className="arrow">
                         more...
                     </a> */}
-                    <div className="social-buttons">
-                        <SocialLinks />
-                    </div>
-                </div>
+					<div className="social-buttons">
+						<SocialLinks />
+					</div>
+				</div>
 
-                {/* <PortfolioList /> */}
-                <BlogList />
+				{/* <PortfolioList /> */}
+				<div className="">
+					<BlogList />
+					<section id="blog" className="container">
+						<div className="box">
+							<div className="content">
+								<Helmet>
+									<script async src="https://platform.twitter.com/widgets.js" charset="utf-8" />
+								</Helmet>
+							{this.props.data.site.siteMetadata.darkmode ? (
+								<a class="twitter-timeline" href="https://twitter.com/JohanGace?ref_src=twsrc%5Etfw">
+									Tweets 
+								</a>
+							) : (
+								<a
+									class="twitter-timeline"
+									// data-theme="dark"
+									href="https://twitter.com/JohanGace?ref_src=twsrc%5Etfw"
+								>
+									Tweets 
+								</a>
+							)}
+												</div>
 
-                {/* <Contact /> */}
-            </Layout>
-        ) : null;
-    }
+						</div>
+					</section>
+				</div>
+
+				{/* <Contact /> */}
+			</Layout>
+		) : null;
+	}
 }
 
 export default IndexPage;
 
 export const query = graphql`
-    query {
-        site {
-            siteMetadata {
-                title
-                capitalizeTitleOnHome
-                titleImage
-                introTag
-                description
-                social {
-                    name
-                    url
-                    icon
-                }
-            }
-        }
-    }
+	query {
+		site {
+			siteMetadata {
+                darkmode
+				title
+				capitalizeTitleOnHome
+				titleImage
+				introTag
+				description
+				social {
+					name
+					url
+					icon
+				}
+			}
+		}
+	}
 `;
